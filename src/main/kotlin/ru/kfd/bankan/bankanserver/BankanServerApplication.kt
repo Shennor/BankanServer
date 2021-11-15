49b82ef20b8c6fcb0caa7aacb039eb289950f5de
Nikita Alexandrov <nikitosiks338@gmail.com>
2021-11-15 21:58:52 +0300

    data classes moved from "main" to separated class

--- src/main/kotlin/ru/kfd/bankan/bankanserver/BankanServerApplication.kt
+++ src/main/kotlin/ru/kfd/bankan/bankanserver/BankanServerApplication.kt
@@ -3,7 +3,6 @@ package ru.kfd.bankan.bankanserver
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.stereotype.Repository
-import java.util.*
 
 @SpringBootApplication
 class BankanServerApplication
@@ -12,64 +11,8 @@ fun main(args: Array<String>) {
     runApplication<BankanServerApplication>(*args)
 }
 
-// Notes about business-logic
-
-//  info collects all visible attributes
-
-// board class
-
-data class BoardInfo(
-    val id: UInt,
-    val name: String,
-    val description: String,
-    val creationDate: Date,
-    val usersIds: List<UInt>
-)
-
-// Board is returned by request ReadBoard
-data class Board(
-    val lists: List<BListInfo>,
-    val listsOrder: List<UInt>
-)
-
-// list class
-
-data class BListInfo(
-    val id: UInt,
-    val name: String,
-    val creationDate: Date
-)
-
-// BList is returned by request ReadList
-data class BList(
-    val cards: List<CardInfo>,
-    val cardsIdsOrder: List<UInt>
-)
-
-// card class
-
-data class CardInfo(
-    val id: UInt,
-    val name: String,
-    val color: UInt,
-    val creationDate: Date,
-    val deadlineDate: Date,
-    val creatorId: UInt,
-    val executorsIds: List<UInt>
-)
-
-sealed class Content {
-    data class Text(val text: String) : Content()
-    abstract class CheckList() : Content()
-    abstract class Image() : Content()
-    //...
-}
-
-data class Card(
-    val contentList: List<Content>
-)
-
 // board repository
+// TODO Repositories whatever it is
 
 @Repository
 class BoardRepository // TODO
@@ -87,7 +30,8 @@ class CardRepository // TODO
 // controllers
 
 
-/* TODO
+/* TODO Database markup
+
    CardTable
    CardId | Name | Color | CreationDate | DeadlineDate | CreatorId | -assignedUserIds- | CardContent
 

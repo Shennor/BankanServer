f814add89a00a1c8092c430d3163a04b46ee4dcd
ariel <nikitosiks338@gmail.com>
2022-02-13 19:07:13 +0300

    work

--- src/main/kotlin/ru/kfd/bankan/bankanserver/repository/BoardRepository.kt
+++ src/main/kotlin/ru/kfd/bankan/bankanserver/repository/BoardRepository.kt
@@ -4,9 +4,5 @@ import org.springframework.data.repository.CrudRepository
 import ru.kfd.bankan.bankanserver.entity.BoardEntity
 
 interface BoardRepository : CrudRepository<BoardEntity, Int> {
-    fun editName(id: Int, new_name: String): Unit {
-        TODO()
-    }
-
-    fun getById(id: Int) : BoardEntity;
+    fun getById(id: Int): BoardEntity
 }

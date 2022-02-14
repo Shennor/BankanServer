15dad61c087fe46550b008f13fb2cc55b290926a
ariel <nikitosiks338@gmail.com>
2022-02-14 21:10:08 +0300

    Some fixes in Lists.kt

--- src/main/kotlin/ru/kfd/bankan/bankanserver/controller/data/Boards.kt
+++ src/main/kotlin/ru/kfd/bankan/bankanserver/controller/data/Boards.kt
@@ -2,9 +2,15 @@ package ru.kfd.bankan.bankanserver.controller.data
 
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import ru.kfd.bankan.bankanserver.repository.BoardRepository
+import ru.kfd.bankan.bankanserver.repository.BoardToListMappingRepository
 
 
 // TODO
 @RequestMapping("/api/board")
 @RestController
-class Boards
+class Boards(
+    val boardRepository: BoardRepository,
+    val boardToListMappingRepository: BoardToListMappingRepository
+) {
+}
